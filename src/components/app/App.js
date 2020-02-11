2200effda9fddd351bd12a4996c9b47feb579c3b
unknown <@15ISPOLATOV.tservicegroup.ru>
2020-02-11 22:24:07 +0300

    #project1-melody-step2 fix path

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import WelcomeScreen from './welcome-screen.jsx';
+import WelcomeScreen from '../welcome-screen/WelcomeScreen';
 
 // eslint-disable-next-line react/prop-types
 export const App = ({errorsCount}) => <WelcomeScreen errorsCount={errorsCount} />;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./components/App";
+import {App} from "../src/components/app/App";
 
 const errorsCount = 999;
 
