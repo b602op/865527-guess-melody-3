ae99e6230411e9b0659434b6166bab74bc8591bf
unknown <@15ISPOLATOV.tservicegroup.ru>
2020-02-13 20:50:44 +0300

    #JS3-2-1 fix

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import WelcomeScreen from '../welcome-screen/WelcomeScreen';
+import WelcomeScreen from '../welcome-screen/welcome-screen';
+
+export const App = ({errorsCount}) => <WelcomeScreen errorsCount={errorsCount} />;
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
 };
-
-export const App = ({errorsCount}) => <WelcomeScreen errorsCount={errorsCount} />;
-
